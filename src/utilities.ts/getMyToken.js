43801cfc401b15b2ad5bb090531e99fbfdf033a0
Ahmed Samy <ahmedsamywezoo@gmail.com>
2025-09-25 16:03:20 +0300

    AUTH

--- src/utilities.ts/getMyToken.js
+++ src/utilities.ts/getMyToken.js
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 
 export default async function getMyToken() {
   let decodecToken = (await cookies()).get("next-auth.session-token")?.value;
-  let token = await decode({token: decodecToken,secret: process.env.AUTH_SECRET});
+  let token = await decode({token: decodecToken,secret: process.env.NEXTAUTH_SECRET});
 
   return token?.token
 }
